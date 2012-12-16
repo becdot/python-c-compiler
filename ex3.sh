e41b926f3001a17c59661b43b01331dea49ca7e4
Becca Liss <becca.liss@gmail.com>
2012-12-16 15:41:11 -0500

    Update gitignore, sh files
     - Remove specific sh files (usage: ex.sh <ex[i]>)

--- ex3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Produce test outputs for ex1.py, given inputs in ex1.test-input;
-# then make sure they're the same as expected.
-# This is our top-level test script.
-
-python ex2.py >ex2.c &&
-gcc -o ex2 ex2.c &&
-./ex2 <ex2.test-input >ex2.test-output
-diff -u ex2.expected-output ex2.test-output
